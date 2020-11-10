497c8f35569a86aa1e836d38cbd5b1e3e428a59b
alceba <admin@alceba.com>
2020-11-10 20:53:58 -0300

    Add mail API.

---
+++ js/email.js
@@ -0,0 +1,25 @@
+
+
+
+function sendEmailAddress() {
+	var data = JSON.stringify({
+	  "email": document.getElementById("inputEmail").value,
+	});
+
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = false;
+
+	xhr.addEventListener("readystatechange", function () {
+	  if (this.readyState === 4) {
+	    console.log(this.responseText);
+	  }
+	});
+
+	xhr.open("POST", "https://alcebanewsletter-c774.restdb.io/rest/emails");
+	xhr.setRequestHeader("content-type", "application/json");
+	xhr.setRequestHeader("x-apikey", "5fab221c863959728838523a");
+	xhr.setRequestHeader("cache-control", "no-cache");
+
+	xhr.send(data);
+}
+
