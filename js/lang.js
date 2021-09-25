5fe235eb640e7b341f11044631b218561473ac7a
alceba <admin@alceba.com>
2021-09-25 15:25:39 -0300

    Reimplementing a new language handler.

--- js/lang.js
+++ js/lang.js
@@ -1,25 +1,34 @@
 
-function changeLanguage(language) {
+function loadLanguage(lang) {
+	var json = null;
 	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-	    if (this.readyState === 4 && this.status === 200) {
-		langDocument = JSON.parse(this.responseText);
-
-		var tags = document.querySelectorAll('a,p,button,input,h2,h4');
-		Array.from(tags).forEach(function(value, index){
-	       		var key = value.dataset.langkey;
-			if(langDocument[key]) {
-				if (value.getAttribute("type") == "email") {
-					value.placeholder = langDocument[key];
-				} else {
-					value.innerHTML = langDocument[key];
-				}
-			}
-		});
-	    }
-	};
-	xhttp.open("GET", "assets/json/" + language + ".json", true);
+	xhttp.open("GET", "assets/json/"+lang+".json", false);
 	xhttp.setRequestHeader("Content-type", "application/json");
 	xhttp.send();
+	if (xhttp.readyState === 4 && xhttp.status === 200) {
+		json = JSON.parse(xhttp.responseText);
+	}
+	return json;
 }
 
+function updateLanguage(tags) {
+	Array.from(tags).forEach(function(key, index){
+		var e = $('[data-langkey="'+key+'"]')[0];
+		if (e.getAttribute("type") == "email") {
+			e.placeholder = data_lang[lang][key];
+		} else {
+			e.innerHTML = data_lang[lang][key];
+		}
+	});
+}
+
+function changeLanguage(language) {
+	lang = language;
+	updateLanguage(Object.keys(data_lang[lang]));
+}
+
+var data_lang = new Object();
+data_lang["en"] = loadLanguage("en");
+data_lang["es"] = loadLanguage("es");
+
+var lang = "en";
